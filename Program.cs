aa473da97cfd3aa059176b81b7dc3faf70db795d
Ngô Trung Kiên <kienhp1hp@gmail.com>
2020-05-17 00:45:07 +0700

    UPDATE 17.5.2020

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ using System.Threading;
 
 class Server
 {
-    
-
-    const int MAX_CONNECTION = 100;
     const int PORT_NUMBER = 9999;
 
     static TcpListener listener;
@@ -31,37 +28,63 @@ class Server
         {"10","Ten"},
     };
 
-    public static void Main()
+    public static void Main(string []args)
     {
+        int MAX_CONNECTION = Convert.ToInt32(args[0]);
         IPAddress address = IPAddress.Parse("127.0.0.1");
 
         listener = new TcpListener(address, PORT_NUMBER);
         Console.WriteLine("SERVER MULTI CLIENT CONNECTION - CODING BY NTK - 74458");
+        Console.WriteLine("IP:Port of Server : " + address + ":" + PORT_NUMBER);
         Console.WriteLine("Waiting for connection...");
         listener.Start();
 
-        for (int i = 0; i < MAX_CONNECTION; i++)
+        for (int i = 1; i <= MAX_CONNECTION+1; i++)
         {
-            Socket soc = listener.AcceptSocket();
-            StringBuilder sb = new StringBuilder();
-            StringBuilder sb1 = new StringBuilder();
-            string IPConnected = sb1.ToString();
-            if (IPConnected.Contains(soc.RemoteEndPoint.ToString()))
+            if (i <= MAX_CONNECTION)
             {
-                Console.WriteLine("429 Many Request");
-                sb.Append("IP:Port of Client: " + soc.RemoteEndPoint + ";" + "Disconnect At: " + DateTime.Now+";Reason : 429 Many Request");
-                File.AppendAllText("D://Access.log", sb.ToString());
-                sb.Clear();
+                new Thread(DoWork).Start();
             }
             else
             {
-                new Thread(DoWork).Start();
-                sb1.Append(soc.RemoteEndPoint);
-                File.AppendAllText("ConnectedIP.txt", sb1.ToString());
+                new Thread(DoWork1).Start();
             }
+
+
+            //Socket soc = listener.AcceptSocket();
+            //StringBuilder sb = new StringBuilder();
+            //StringBuilder sb1 = new StringBuilder();
+            //string IPConnected = sb1.ToString();
+            //if (IPConnected.Contains(soc.RemoteEndPoint.ToString()))
+            //{
+            //    Console.WriteLine("429 Many Request");
+            //    sb.Append("IP:Port of Client: " + soc.RemoteEndPoint + ";" + "Disconnect At: " + DateTime.Now+";Reason : 429 Many Request");
+            //    File.AppendAllText("D://Access.log", sb.ToString());
+            //    sb.Clear();
+            //}
+            //else
+            //{
+            //    new Thread(DoWork).Start();
+            //    sb1.Append(soc.RemoteEndPoint);
+            //    File.AppendAllText("ConnectedIP.txt", sb1.ToString());
+            //}
         }
     }
-
+    static void DoWork1()
+    {
+        while (true)
+        {
+            Socket soc = listener.AcceptSocket();
+            var stream = new NetworkStream(soc);
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+            Console.WriteLine("Connection received from(DENIED): {0}",
+                                  soc.RemoteEndPoint);
+            writer.WriteLine("503 Service Unavalible, Close Connection !");
+            //soc.Close();
+        }
+    }
     static void DoWork()
     {
         while (true)
@@ -102,8 +125,16 @@ class Server
 
                     if (_data.ContainsKey(id))
                         writer.WriteLine("Number you've entered: '{0}'", _data[id]);
+                    else if (id.Contains("dig"))
+                    {
+                        string[] idcut = id.Split(' ');
+                        var ipaddress = Dns.GetHostAddresses(idcut[1])[0];
+                        writer.WriteLine("IP of Domain is : " + ipaddress);
+                    }
                     else
-                        writer.WriteLine("Number is not valid !");
+                    {
+                        writer.WriteLine("Wrong Syntax ! ");
+                    }
                 }
                 stream.Close();
             }
